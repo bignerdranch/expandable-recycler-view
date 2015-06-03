981445df264b22ad35748dfd033e505cd2a4a635
rbro112 <r.brooks112@yahoo.com>
2015-06-03 10:30:21 -0400

    Added toolbar throughout. Added back navigation in vertical sample. Added checkbox to turn animation on or off

--- app/src/main/java/com/ryanbrooks/expandablerecyclerviewsample/MainActivity.java
+++ app/src/main/java/com/ryanbrooks/expandablerecyclerviewsample/MainActivity.java
@@ -3,29 +3,38 @@ package com.ryanbrooks.expandablerecyclerviewsample;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.ryanbrooks.expandablerecyclerviewsample.VerticalLinearRecyclerViewSample.VerticalLinearRecyclerViewSample;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 /**
  * Created by Ryan Brooks on 5/19/15.
  */
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button mVerticalSampleButton;
-    private Button mHorizontalSampleButton;
-    private Button mGridSampleButton;
+    @InjectView(R.id.main_vertical_linear_button)
+    Button mVerticalSampleButton;
+    @InjectView(R.id.main_horizontal_linear_button)
+    Button mHorizontalSampleButton;
+    @InjectView(R.id.main_grid_button)
+    Button mGridSampleButton;
+    @InjectView(R.id.activity_main_toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
 
-        mVerticalSampleButton = (Button) findViewById(R.id.main_vertical_linear_button);
-        mHorizontalSampleButton = (Button) findViewById(R.id.main_horizontal_linear_button);
-        mGridSampleButton = (Button) findViewById(R.id.main_grid_button);
+        setSupportActionBar(mToolbar);
 
         mVerticalSampleButton.setOnClickListener(this);
         mHorizontalSampleButton.setOnClickListener(this);
--- app/src/main/java/com/ryanbrooks/expandablerecyclerviewsample/VerticalLinearRecyclerViewSample/VerticalLinearRecyclerViewSample.java
+++ app/src/main/java/com/ryanbrooks/expandablerecyclerviewsample/VerticalLinearRecyclerViewSample/VerticalLinearRecyclerViewSample.java
@@ -1,13 +1,11 @@
 package com.ryanbrooks.expandablerecyclerviewsample.VerticalLinearRecyclerViewSample;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Spinner;
 
 import com.ryanbrooks.expandablerecyclerview.Model.ExpandingObject;
@@ -26,11 +24,13 @@ public class VerticalLinearRecyclerViewSample extends AppCompatActivity {
 
     private MyExpandableAdapter mExpandableAdapter;
 
-    @InjectView(R.id.toolbar)
+    @InjectView(R.id.vertical_sample_toolbar)
     Toolbar mToolbar;
     @InjectView(R.id.vertical_recyclerview_sample)
     RecyclerView mRecyclerView;
-    @InjectView(R.id.toolbar_spinner)
+    @InjectView(R.id.vertical_sample_toolbar_checkbox)
+    CheckBox mAnimationEnabledCheckBox;
+    @InjectView(R.id.vertical_sample_toolbar_spinner)
     Spinner mToolbarSpinner;
 
     @Override
@@ -41,6 +41,7 @@ public class VerticalLinearRecyclerViewSample extends AppCompatActivity {
 
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mExpandableAdapter = new MyExpandableAdapter(this, setUpTestData(20));
         mRecyclerView.setAdapter(mExpandableAdapter);
