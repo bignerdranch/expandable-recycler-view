7d4addf38eead09762a312380c86896b2ffc3f92
rbro112 <r.brooks112@yahoo.com>
2015-06-03 10:55:35 -0400

    Handled the case if the map is null. It is now initially set to empty and both a null and empty check are done on the map after it is serialized in. If one or the other is false, it will move on and regenerate the Map from the initial list

--- expandablerecyclerview/src/main/java/com/ryanbrooks/expandablerecyclerview/Adapter/ExpandableRecyclerAdapter.java
+++ expandablerecyclerview/src/main/java/com/ryanbrooks/expandablerecyclerview/Adapter/ExpandableRecyclerAdapter.java
@@ -128,8 +128,9 @@ public abstract class ExpandableRecyclerAdapter extends RecyclerView.Adapter<Rec
         if (savedInstanceStateBundle == null) {
             return;
         }
+        mStableIdMap = new HashMap<>();
         mStableIdMap = (HashMap<Integer, Boolean>) savedInstanceStateBundle.getSerializable(STABLE_ID_MAP);
-        if (mStableIdMap != null) {
+        if (mStableIdMap != null || !mStableIdMap.isEmpty()) {
             int i = 0;
             while (i < mItemList.size()) {
                 if (mItemList.get(i) instanceof ParentObject) {
@@ -147,6 +148,8 @@ public abstract class ExpandableRecyclerAdapter extends RecyclerView.Adapter<Rec
                 i++;
             }
             notifyDataSetChanged();
+        } else {
+            mStableIdMap = generateStableIdMapFromList(mItemList);
         }
     }
 }
