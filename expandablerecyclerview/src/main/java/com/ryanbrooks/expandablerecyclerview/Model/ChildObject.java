126cacf4353c766c8ca4400d68b2a3e762f17714
rbro112 <r.brooks112@yahoo.com>
2015-06-05 11:44:50 -0400

    Initial commit and changed ChildObject to interface

--- expandablerecyclerview/src/main/java/com/ryanbrooks/expandablerecyclerview/Model/ChildObject.java
+++ expandablerecyclerview/src/main/java/com/ryanbrooks/expandablerecyclerview/Model/ChildObject.java
@@ -4,5 +4,6 @@ package com.ryanbrooks.expandablerecyclerview.Model;
  * Created by Ryan Brooks on 5/27/15.
  * This is more of a placeholder than anything. Here to assure user extends it
  */
-public class ChildObject extends ExpandingObject {
+public interface ChildObject {
+    
 }
