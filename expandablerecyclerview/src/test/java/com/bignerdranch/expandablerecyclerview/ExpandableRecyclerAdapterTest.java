9de4a00240617465498563ae2b837916a1980c5c
paul <pturner@bignerdranch.com>
2016-09-23 10:14:29 -0400

    rename parentListItem to BaseParentItems, verify notify calls

--- expandablerecyclerview/src/test/java/com/bignerdranch/expandablerecyclerview/ExpandableRecyclerAdapterTest.java
+++ expandablerecyclerview/src/test/java/com/bignerdranch/expandablerecyclerview/ExpandableRecyclerAdapterTest.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.expandablerecyclerview;
 
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView.AdapterDataObserver;
 import android.view.ViewGroup;
 
 import com.bignerdranch.expandablerecyclerview.model.ParentListItem;
@@ -14,7 +15,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class ExpandableRecyclerAdapterTest {
@@ -47,11 +51,12 @@ public class ExpandableRecyclerAdapterTest {
     }
 
     private TestExpandableRecyclerAdapter mExpandableRecyclerAdapter;
-    private List<ParentListItem> mParentListItems;
+    private List<ParentListItem> mBaseParentItems;
+    private AdapterDataObserver mDataObserver;
 
     @Before
     public void setup() throws NoSuchFieldException, IllegalAccessException {
-        mParentListItems = new ArrayList<>();
+        mBaseParentItems = new ArrayList<>();
 
         for (int i = 0; i < 10; i++) {
             List<Object> childObjects = new ArrayList<>();
@@ -63,11 +68,11 @@ public class ExpandableRecyclerAdapterTest {
             Mockito.<List<?>>when(parentListItem.getChildItemList()).thenReturn(childObjects);
             when(parentListItem.isInitiallyExpanded()).thenReturn(i % 2 == 0); // Every other parent will return true
 
-            mParentListItems.add(parentListItem);
+            mBaseParentItems.add(parentListItem);
         }
 
-        mExpandableRecyclerAdapter = new TestExpandableRecyclerAdapter(mParentListItems);
-        TestUtils.fixAdapterForTesting(mExpandableRecyclerAdapter);
+        mExpandableRecyclerAdapter = new TestExpandableRecyclerAdapter(mBaseParentItems);
+        mDataObserver = TestUtils.fixAdapterForTesting(mExpandableRecyclerAdapter);
     }
 
     @Test
@@ -77,27 +82,27 @@ public class ExpandableRecyclerAdapterTest {
 
     @Test
     public void adapterCorrectlyInitializesExpandedParentsWhenAllParentItemsAreInitiallyCollapsed() {
-        for (ParentListItem parentListItem : mParentListItems) {
+        for (ParentListItem parentListItem : mBaseParentItems) {
             when(parentListItem.isInitiallyExpanded()).thenReturn(false);
         }
-        mExpandableRecyclerAdapter = new TestExpandableRecyclerAdapter(mParentListItems);
+        mExpandableRecyclerAdapter = new TestExpandableRecyclerAdapter(mBaseParentItems);
 
         assertEquals(10, mExpandableRecyclerAdapter.getItemCount());
     }
 
     @Test
     public void adapterCorrectlyInitializesExpandedParentsWhenAllParentItemsAreInitiallyExpanded() {
-        for (ParentListItem parentListItem : mParentListItems) {
+        for (ParentListItem parentListItem : mBaseParentItems) {
             when(parentListItem.isInitiallyExpanded()).thenReturn(true);
         }
-        mExpandableRecyclerAdapter = new TestExpandableRecyclerAdapter(mParentListItems);
+        mExpandableRecyclerAdapter = new TestExpandableRecyclerAdapter(mBaseParentItems);
 
         assertEquals(40, mExpandableRecyclerAdapter.getItemCount());
     }
 
     @Test
     public void collapsingExpandedParentRemovesChildren() {
-        ParentListItem firstParentListItem = mParentListItems.get(0);
+        ParentListItem firstParentListItem = mBaseParentItems.get(0);
         ParentWrapper parentWrapper = (ParentWrapper) mExpandableRecyclerAdapter.getListItem(0);
 
         assertEquals(25, mExpandableRecyclerAdapter.getItemCount());
@@ -108,11 +113,13 @@ public class ExpandableRecyclerAdapterTest {
 
         mExpandableRecyclerAdapter.collapseParent(0);
 
+        verify(mDataObserver).onItemRangeRemoved(1, 3);
+
         assertEquals(22, mExpandableRecyclerAdapter.getItemCount());
         parentWrapper = (ParentWrapper) mExpandableRecyclerAdapter.getListItem(0);
-        assertEquals(mParentListItems.get(0), parentWrapper.getParentListItem());
+        assertEquals(mBaseParentItems.get(0), parentWrapper.getParentListItem());
         parentWrapper = (ParentWrapper) mExpandableRecyclerAdapter.getListItem(1);
-        assertEquals(mParentListItems.get(1), parentWrapper.getParentListItem());
+        assertEquals(mBaseParentItems.get(1), parentWrapper.getParentListItem());
     }
 
 
@@ -120,16 +127,18 @@ public class ExpandableRecyclerAdapterTest {
     public void collapsingCollapsedParentHasNoEffect() {
         assertEquals(25, mExpandableRecyclerAdapter.getItemCount());
         ParentWrapper parentWrapper = (ParentWrapper) mExpandableRecyclerAdapter.getListItem(4);
-        assertEquals(mParentListItems.get(1), parentWrapper.getParentListItem());
+        assertEquals(mBaseParentItems.get(1), parentWrapper.getParentListItem());
         parentWrapper = (ParentWrapper) mExpandableRecyclerAdapter.getListItem(5);
-        assertEquals(mParentListItems.get(2), parentWrapper.getParentListItem());
+        assertEquals(mBaseParentItems.get(2), parentWrapper.getParentListItem());
 
         mExpandableRecyclerAdapter.collapseParent(1);
 
+        verify(mDataObserver, never()).onItemRangeRemoved(anyInt(), anyInt());
+
         assertEquals(25, mExpandableRecyclerAdapter.getItemCount());
         parentWrapper = (ParentWrapper) mExpandableRecyclerAdapter.getListItem(4);
-        assertEquals(mParentListItems.get(1), parentWrapper.getParentListItem());
+        assertEquals(mBaseParentItems.get(1), parentWrapper.getParentListItem());
         parentWrapper = (ParentWrapper) mExpandableRecyclerAdapter.getListItem(5);
-        assertEquals(mParentListItems.get(2), parentWrapper.getParentListItem());
+        assertEquals(mBaseParentItems.get(2), parentWrapper.getParentListItem());
     }
 }
